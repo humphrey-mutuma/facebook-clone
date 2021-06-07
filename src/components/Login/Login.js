5ec1d1ef9dfa825e8bd1a3f0cfa7dd1952f2038d
humphrey-mutuma <humphreymutuma01@gmail.com>
2021-06-07 15:41:34 +0300

    Successfully Add google login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,13 +1,30 @@
 import { Button, Link } from "@material-ui/core";
 import { Facebook } from "@material-ui/icons";
 import "./Login.css";
+
+import { auth, provider } from "./../../firebase";
+
 const Login = () => {
+  const signIn = () => {
+    auth
+      .signInWithPopup(provider)
+      .then((results) => {
+        console.log(results);
+      })
+      .catch((error) => alert.log(error.message));
+  };
   return (
     <div className="login">
       <article>
         <Facebook className="login__logo" color="primary" />
         <p>Facebook</p>
-        <Button className="login__btn" variant="outlined" color="primary">
+        <Button
+          type="submit"
+          onClick={signIn}
+          className="login__btn"
+          variant="outlined"
+          color="primary"
+        >
           Login to Facebook
         </Button>
       </article>
